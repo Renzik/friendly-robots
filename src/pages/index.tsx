11e943cdd76569648f1800812ba51009ad5af27e
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2022-06-26 03:55:36 -0400

    Setting up Prisma

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import { trpc } from "@/utils/trpc";
 import type { NextPage } from "next";
+import { prisma } from "@/db/client";
 
-const Home: NextPage = () => {
+const Home: NextPage = (props: any) => {
   const { data, isLoading } = trpc.useQuery(["hello"]);
 
   if (isLoading || !data) return <div>Loading...</div>;
 
-  return <div className="text-2xl font-bold">{data.greeting}</div>;
+  return <div className="text-2xl font-bold">{props.robots}</div>;
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  const robots = await prisma.robot.findMany();
+
+  return {
+    props: {
+      robots: JSON.stringify(robots),
+    },
+  };
+};
