36a180ac9889edb6505e98c90081246ad29e2f76
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2022-06-26 04:21:24 -0400

    tRPC first call to db successful

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,21 +3,15 @@ import type { NextPage } from "next";
 import { prisma } from "@/db/client";
 
 const Home: NextPage = (props: any) => {
-  const { data, isLoading } = trpc.useQuery(["hello"]);
+  const { data, isLoading } = trpc.useQuery(["getAllRobots"]);
 
-  // if (isLoading || !data) return <div>Loading...</div>;
+  if (isLoading || !data) return <div>Loading...</div>;
 
-  return <div className="text-2xl font-bold">{props.robots}</div>;
+  return (
+    <div className="text-2xl font-bold">
+      <div>{data[0]?.name}</div>
+    </div>
+  );
 };
 
 export default Home;
-
-export const getServerSideProps = async () => {
-  const robots = await prisma.robot.findMany();
-
-  return {
-    props: {
-      robots: JSON.stringify(robots),
-    },
-  };
-};
